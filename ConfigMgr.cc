74ab7f81fd36efb16f530a0517c91f54feea0c1f
Nick Sampsell <nick@nicksampsell.com>
2023-03-12 20:02:20 -0400

    Settings.json file added to /boot/ and reader now functional

--- ConfigMgr.cc
+++ ConfigMgr.cc
@@ -76,3 +76,50 @@ std::vector<std::vector<uint8_t>> ConfigMgr::getColors(std::string theme)
     
     return colors;
 }
+
+Config ConfigMgr::getConfig()
+{
+    std::ifstream f("/boot/sign/settings.json");
+    json allSettings = json::parse(f);
+
+    Config c;
+    c.iconPath = allSettings["iconPath"].get<std::string>();
+    c.defaultTheme = allSettings["defaultTheme"].get<std::string>();
+    c.useSpecialThemes = allSettings["useSpecialThemes"].get<bool>();
+    c.showLongDate = allSettings["features"]["showLongDate"].get<bool>();
+    c.showShortDate = allSettings["features"]["showShortDate"].get<bool>();
+    c.showSpecials = allSettings["features"]["showSpecials"].get<bool>();
+    c.showBirthdays = allSettings["features"]["showBirthdays"].get<bool>();
+    c.showWeather = allSettings["features"]["showWeather"].get<bool>();
+    c.showTime = allSettings["features"]["showTime"].get<bool>();
+    c.rotateInfo = allSettings["features"]["rotateInfo"].get<bool>();
+    c.useAnimation = allSettings["features"]["useAnimation"].get<bool>();
+    c.maxSpecialsPerDay = allSettings["features"]["maxSpecialsPerDay"].get<int>();
+   
+    
+    std::vector<ThemeCriteria> tcVector;
+    for(json::iterator it = allSettings["themeCriteria"].begin(); it != allSettings["themeCriteria"].end(); ++it)
+    {
+        ThemeCriteria tcItem;
+        tcItem.Theme = it.key();
+        
+        std::map<std::string, std::string> criteria;
+
+        for(json::iterator it2 = it.value().begin(); it2 != it.value().end(); ++it2)
+        {
+            criteria.insert(std::pair<std::string, std::string>(it2.key(), it2.value()));
+        }
+        tcItem.Criteria = criteria;
+        tcVector.push_back(tcItem);
+    }
+
+    c.themeCriteria = tcVector;
+    c.specialsCycle = { 
+        allSettings["cycle"]["type"].get<std::string>(),
+        allSettings["cycle"]["daysInCycle"].get<int>()
+    };
+
+    return c;
+}
+
+
--- ConfigMgr.h
+++ ConfigMgr.h
@@ -24,10 +24,39 @@ struct Special {
     std::string Icon;
 };
 
+struct ThemeCriteria {
+    std::string Theme;
+    std::map<std::string, std::string> Criteria;
+};
+
+struct Cycle
+{
+    std::string Type;
+    int DaysInCycle;
+};
+
+struct Config {
+    std::string iconPath;
+    std::string defaultTheme;
+    bool useSpecialThemes;
+    std::vector<ThemeCriteria> themeCriteria;
+    bool showLongDate;
+    bool showShortDate;
+    bool showSpecials;
+    bool showBirthdays;
+    bool showWeather;
+    bool showTime;
+    bool rotateInfo;
+    bool useAnimation;
+    int maxSpecialsPerDay;
+    Cycle specialsCycle;
+};
+
 class ConfigMgr {
     public:
         ConfigMgr();
         std::vector<std::vector<uint8_t>> getColors(std::string theme);
         std::map<std::string, std::vector<Special>> getSpecials();
+        Config getConfig();
 };
 #endif
